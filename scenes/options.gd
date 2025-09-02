226712cee82a6b078161fd3874ea7148866eb53e
brian <brianogarciav@gmail>
2025-09-02 08:55:53 -0300

    Menu

---
+++ scenes/options.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_languaje_pressed() -> void:
+	pass # Replace with function body.
+	
+
+func _on_volume_pressed() -> void:
+	pass # Replace with function body.
+	
+
+func _on_return_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
